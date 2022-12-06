f652825d1ee6731552a67d754fa52e519fbcf7ae
Tom Hummel <tohu@hey.com>
2022-12-05 22:07:40 -0800

    Add proxied cname records targeting s3 static sites

--- tomhummel.com/main.tf
+++ tomhummel.com/main.tf
@@ -25,3 +25,17 @@ resource "cloudflare_zone" "tomhummel_com" {
   account_id = var.tomhummel_com_account_id
   zone       = "tomhummel.com"
 }
+
+resource "cloudflare_record" "cname-proxied" {
+  for_each = {
+    "data"          = "data.tomhummel.com.s3-website-us-east-1.amazonaws.com"
+    "tomhummel.com" = "tomhummel.com.s3-website-us-east-1.amazonaws.com"
+    "www"           = "www.tomhummel.com.s3-website-us-east-1.amazonaws.com"
+  }
+  zone_id = cloudflare_zone.tomhummel_com.id
+  name    = each.key
+  value   = each.value
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
