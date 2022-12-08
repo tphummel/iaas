e3a39a152fba08c8f3d420caeaa2730ef2df4f6f
Tom Hummel <tohu@hey.com>
2022-12-07 21:27:58 -0800

    Add wordle website resources

--- tomhummel.com/main.tf
+++ tomhummel.com/main.tf
@@ -39,3 +39,56 @@ resource "cloudflare_record" "cname-proxied" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_pages_project" "wordle" {
+  account_id        = var.tomhummel_com_account_id
+  name              = "wordle"
+  production_branch = "main"
+  build_config {
+    build_command   = "git submodule update --init --recursive && hugo"
+    destination_dir = "public"
+    root_dir        = ""
+  }
+  source {
+    type = "github"
+    config {
+      owner                         = "tphummel"
+      repo_name                     = "wordle-static"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "all"
+      preview_branch_includes       = ["*"]
+    }
+  }
+  deployment_configs {
+    preview {
+      environment_variables = {
+        HUGO_VERSION = "0.87.0"
+      }
+      compatibility_date = "2022-08-15"
+    }
+    production {
+      environment_variables = {
+        HUGO_VERSION = "0.87.0"
+      }
+      compatibility_date = "2022-08-16"
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "wordle" {
+  account_id   = var.tomhummel_com_account_id
+  project_name = cloudflare_pages_project.wordle.name
+  domain       = "wordle.tomhummel.com"
+}
+
+resource "cloudflare_record" "wordle" {
+  zone_id = cloudflare_zone.tomhummel_com.id
+  name    = cloudflare_pages_domain.wordle.domain
+  value   = cloudflare_pages_project.wordle.subdomain
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
