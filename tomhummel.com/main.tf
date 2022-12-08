767818bf021c7aff4b72ec25394a1bf3b0e5eba3
Tom Hummel <tohu@hey.com>
2022-12-07 21:34:00 -0800

    Add movies site

--- tomhummel.com/main.tf
+++ tomhummel.com/main.tf
@@ -92,3 +92,59 @@ resource "cloudflare_record" "wordle" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_pages_project" "movies" {
+  account_id        = var.tomhummel_com_account_id
+  name              = "movies"
+  production_branch = "main"
+  build_config {
+    build_command   = "hugo"
+    destination_dir = "public"
+    root_dir        = ""
+  }
+  source {
+    type = "github"
+    config {
+      owner                         = "tphummel"
+      repo_name                     = "movies"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "all"
+      preview_branch_includes       = ["*"]
+    }
+  }
+  deployment_configs {
+    preview {
+      environment_variables = {
+        HUGO_VERSION = "0.101.0"
+      }
+      compatibility_date = "2022-08-15"
+    }
+    production {
+      environment_variables = {
+        HUGO_VERSION = "0.101.0"
+      }
+      r2_buckets = {
+        "R2" = "movies-tomhummel-com-images"
+      }
+      compatibility_date = "2022-08-16"
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "movies" {
+  account_id   = var.tomhummel_com_account_id
+  project_name = cloudflare_pages_project.movies.name
+  domain       = "movies.tomhummel.com"
+}
+
+resource "cloudflare_record" "movies" {
+  zone_id = cloudflare_zone.tomhummel_com.id
+  name    = cloudflare_pages_domain.movies.domain
+  value   = cloudflare_pages_project.movies.subdomain
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
