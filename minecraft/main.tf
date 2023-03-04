03ad4b0366d83d5def50ccbbd49eb321c3df3424
Tom Hummel <tohu@hey.com>
2023-03-04 10:20:59 -0800

    add further setup steps

--- minecraft/main.tf
+++ minecraft/main.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = "= 1.3.7"
   required_providers {
     oci = {
-      source = "oracle/oci"
+      source  = "oracle/oci"
       version = "4.103.0"
     }
   }
@@ -26,19 +26,19 @@ terraform {
 }
 
 provider "oci" {
-  auth = "APIKey"
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid = var.user_ocid
-  fingerprint = var.fingerprint
-  private_key_path = var.private_key_path
-  region = var.region
+  auth                 = "APIKey"
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  region               = var.region
   disable_auto_retries = true
 }
 
 locals {
-  project_name = "minecraft"
-  instance_shape = "VM.Standard.A1.Flex"
-  image_ocid = "ocid1.image.oc1.iad.aaaaaaaabr2p2s6fnh5kf4u77y7se2kmaieuzjhqfmjwquw3csgq32i6kx5a"
+  project_name    = "minecraft"
+  instance_shape  = "VM.Standard.A1.Flex"
+  image_ocid      = "ocid1.image.oc1.iad.aaaaaaaabr2p2s6fnh5kf4u77y7se2kmaieuzjhqfmjwquw3csgq32i6kx5a"
   vcn_subnet_cidr = "10.88.0.0/30"
 }
 
@@ -67,7 +67,7 @@ resource "oci_core_vcn" "mc" {
 resource "oci_core_internet_gateway" "mc" {
   compartment_id = var.tenancy_ocid
   display_name   = local.project_name
-  vcn_id = oci_core_vcn.mc.id
+  vcn_id         = oci_core_vcn.mc.id
 }
 
 resource "oci_core_route_table" "mc" {
@@ -98,7 +98,8 @@ resource "oci_core_security_list" "mc" {
   ingress_security_rules {
     # Options are supported only for ICMP ("1"), TCP ("6"), UDP ("17"), and ICMPv6 ("58").
     protocol = 6
-    source   = "${data.external.current_ip.result.ip}/32"
+    # source   = "0.0.0.0/0"
+    source = "${data.external.current_ip.result.ip}/32"
 
     tcp_options {
       max = 22
@@ -110,7 +111,7 @@ resource "oci_core_security_list" "mc" {
     # Options are supported only for ICMP ("1"), TCP ("6"), UDP ("17"), and ICMPv6 ("58").
     protocol = 6
     source   = "0.0.0.0/0"
-  
+
     tcp_options {
       max = 25565
       min = 25565
@@ -118,14 +119,18 @@ resource "oci_core_security_list" "mc" {
   }
 
   ingress_security_rules {
-    # Options are supported only for ICMP ("1"), TCP ("6"), UDP ("17"), and ICMPv6 ("58").
-    protocol = 17
+    protocol = 17 // udp
     source   = "0.0.0.0/0"
-  
+
     udp_options {
       max = 25565
       min = 25565
     }
+  }
+
+  ingress_security_rules {
+    protocol = 1 // icmp
+    source   = "0.0.0.0/0"
   }
 
   vcn_id = oci_core_vcn.mc.id
@@ -165,9 +170,7 @@ resource "oci_core_instance" "mc" {
     ignore_changes = [source_details[0].source_id]
   }
 
-  metadata = {
-    ssh_authorized_keys = file(var.ssh_public_key_path)
-  }
+
 
   shape = local.instance_shape
 
@@ -178,7 +181,44 @@ resource "oci_core_instance" "mc" {
   }
   shape_config {
     memory_in_gbs = 6
-    ocpus = 1
+    ocpus         = 1
+  }
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_public_key_path)
+    user_data           = <<-EOF
+#!/bin/bash
+# install netcat
+# install htop
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo rpm --import https://yum.corretto.aws/corretto.key
+sudo curl -Lo /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+sudo dnf -y install java-17-amazon-corretto-devel
+sudo wget https://api.papermc.io/v2/projects/paper/versions/1.19.3/builds/381/downloads/paper-1.19.3-381.jar > /home/opc/
+sudo chown opc:opc /home/opc/paper-1.19.3-381.jar
+echo eula=true > eula.txt
+cat <<MCUNIT | sudo tee /etc/systemd/system/minecraft.service
+[Unit]
+Description=Minecraft Server
+
+[Service]
+WorkingDirectory=/home/opc/
+ExecStart=/usr/bin/java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-1.19.3-381.jar --nogui
+User=opc
+Group=opc
+Type=simple
+Restart=on-failure
+RestartSec=30s
+
+[Install]
+WantedBy=multi-user.target
+MCUNIT
+
+sudo systemctl daemon-reload
+sudo systemctl enable minecraft.service
+sudo systemctl start minecraft.service
+
+EOF
   }
 
   timeouts {
@@ -188,26 +228,26 @@ resource "oci_core_instance" "mc" {
 
 resource "oci_objectstorage_bucket" "mc" {
   compartment_id = var.tenancy_ocid
-  name = local.project_name
-  namespace = var.object_storage_namespace
+  name           = local.project_name
+  namespace      = var.object_storage_namespace
 
-  access_type = "NoPublicAccess"
+  access_type           = "NoPublicAccess"
   object_events_enabled = false
-  storage_tier = "Standard"
-  versioning = "Disabled"
+  storage_tier          = "Standard"
+  versioning            = "Disabled"
 }
 
 resource "oci_identity_dynamic_group" "mc" {
   compartment_id = var.tenancy_ocid
-  description = "all compute instances in tenancy"
-  matching_rule = "instance.compartment.id = '${var.tenancy_ocid}'"
-  name = local.project_name
+  description    = "all compute instances in tenancy"
+  matching_rule  = "instance.compartment.id = '${var.tenancy_ocid}'"
+  name           = local.project_name
 }
 
 resource "oci_identity_policy" "mc" {
   compartment_id = var.tenancy_ocid
-  description = local.project_name
-  name = local.project_name
+  description    = local.project_name
+  name           = local.project_name
   statements = [
     "Allow dynamic-group ${oci_identity_dynamic_group.mc.name} to read buckets in tenancy",
     "Allow dynamic-group ${oci_identity_dynamic_group.mc.name} to manage objects in tenancy where any {request.permission='OBJECT_CREATE', request.permission='OBJECT_INSPECT'}"
